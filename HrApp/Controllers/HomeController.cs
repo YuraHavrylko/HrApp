5da7d21e5941d60b356149372e6dfed6a8cc9b72
Natalia <yakymiv.natalia@gmail.com>
2017-09-29 19:06:18 +0300

    added search by job

--- HrApp/Controllers/HomeController.cs
+++ HrApp/Controllers/HomeController.cs
@@ -22,19 +22,17 @@ namespace HrApp.Controllers
         public ActionResult Index()
         {
             var persons = _unitOfWork.PersonRepository.GetAll();
-            int i = 0;
+
             foreach (var person in persons)
             {
-                person.Educations = _unitOfWork.EducationRepository.GetAllWhere(new Education() {PersonId = person.PersonId}).ToList();
+                person.Educations = _unitOfWork.EducationRepository.GetAllWhere(new Education() { PersonId = person.PersonId }).ToList();
                 person.WorkExperiences = _unitOfWork.WorkExpireanceRepository.GetAllWhere(new WorkExperience() { PersonId = person.PersonId }).ToList();
                 person.Jobs = _unitOfWork.JobRepository.GetAllWhere(new Job() { PersonId = person.PersonId }).ToList();
                 person.Interviews = _unitOfWork.InterviewRepository.GetAllWhere(new Interview() { PersonId = person.PersonId }).ToList();
                 person.ProfessionalSkills = _unitOfWork.ProfessionalSkillRepository.GetAllWhere(new ProfessionalSkill() { PersonId = person.PersonId }).ToList();
                 person.Languages = _unitOfWork.LanguageRepository.GetAllWhere(new Language() { PersonId = person.PersonId }).ToList();
                 person.PersonTypeJobs = _unitOfWork.TypeJobRepository.GetAllWhere(new TypeJob() { PersonId = person.PersonId }).ToList();
-                if (i > 10)
-                    break;
-                i++;
+
             }
             return View(persons);
         }
@@ -53,5 +51,31 @@ namespace HrApp.Controllers
 
             return View(person);
         }
+
+        public ActionResult Find(string searchString)
+        {
+            searchString = "in";
+            var personsSearchByJob = _unitOfWork.JobRepository.GetAllWhere(new Job() { JobName = searchString }).ToList();
+            List<Person> persons = new List<Person>();
+            for (int i = 0; i < personsSearchByJob.Count; i++)
+            {
+                persons.Add(_unitOfWork.PersonRepository.Get(personsSearchByJob[i].PersonId.Value));
+            }
+                foreach (var person in persons)
+                {
+                        person.Educations = _unitOfWork.EducationRepository.GetAllWhere(new Education() { PersonId = person.PersonId }).ToList();
+                        person.WorkExperiences = _unitOfWork.WorkExpireanceRepository.GetAllWhere(new WorkExperience() { PersonId = person.PersonId }).ToList();
+                        person.Jobs = _unitOfWork.JobRepository.GetAllWhere(new Job() { PersonId = person.PersonId }).ToList();
+                        person.Interviews = _unitOfWork.InterviewRepository.GetAllWhere(new Interview() { PersonId = person.PersonId }).ToList();
+                        person.ProfessionalSkills = _unitOfWork.ProfessionalSkillRepository.GetAllWhere(new ProfessionalSkill() { PersonId = person.PersonId }).ToList();
+                        person.Languages = _unitOfWork.LanguageRepository.GetAllWhere(new Language() { PersonId = person.PersonId }).ToList();
+                        person.PersonTypeJobs = _unitOfWork.TypeJobRepository.GetAllWhere(new TypeJob() { PersonId = person.PersonId }).ToList();
+
+                    
+                }
+            
+            return View("Index",persons);
+        }
+        
     }
 }
